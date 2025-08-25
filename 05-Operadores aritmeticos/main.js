33ff27c3649dcc2234f3e8701ed9ffdaadee87b9
nelviCastellanos <nelvicas97@gmail.com>
2025-08-24 22:18:48 -0600

    tipos de datos

--- 05-Operadores aritmeticos/main.js
+++ 05-Operadores aritmeticos/main.js
@@ -12,3 +12,86 @@ console.log(myFloat);
 
 typeof myInt;
 console.log(typeof myFloat);
+
+
+// Operadores aritmeticos 
+
+let sum = 10 + 7;
+let resta = 9 * 8;
+let div = 60 % 3;
+
+console.log(sum);
+console.log(resta);
+console.log(div);
+
+//   declaracion de variables 
+
+
+var num1 = 10;
+var num2 = 50;
+9 * num1;
+num2 / num1;
+
+console.log(num1 / num2);
+
+
+console.log(5 + 10 * 3);
+console.log(num2 % 9) * num1;
+console.log(num2 + num1 / 8 + 2);
+
+
+
+// operador de incremento 
+
+var num1 = 4;
+num1++;
+console.log(num1);
+
+//  operador de decremento
+
+var num2 = 6;
+num2--;
+console.log(num2);
+
+
+
+////  Operadores de asignacion
+
+/*
+var x = 3;
+var y = 4;
+x = y;
+console.log(x);
+
+
+
+/*
+
++= adicion asignacion
+-= resta asignacion
+*= multiplicacion asignacion
+/= divicion asignacion
+
+*/
+
+var x = 3; // x contiene el valor 3
+var y = 4; // y contiene el valor 4
+var i = x *= y; // x ahora contiene el valor 12
+console.log(i); 
+
+
+
+
+//   Operadores de comparacion
+
+/*
+
+===   igualdad estricto
+!==   igualdad no estricto
+<     menor que 
+>     mayor que 
+<=    menor o igual que
+>=    mayor o igual que 
+ 
+
+*/
