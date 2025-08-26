36cb11bade65290b14f05ed9fabfa605feaab99f
nelviCastellanos <nelvicas97@gmail.com>
2025-08-25 21:53:01 -0600

    tema string

---
+++ 07-string/main.js
@@ -0,0 +1,31 @@
+var string = "La revolucion no sere televisada";
+console.log(string);
+
+var maltring = string;
+
+console.log(maltring);
+
+
+///   concattenando cadenas 
+
+var one = "Hello, ";
+var two = "how are you?";
+var joined = one + two;
+console.log(joined);
+
+
+
+var multiple = one + one + one + one + two;
+console.log(multiple);
+
+
+/// conversion de tipo de datos
+
+var myString = "123";
+var myNum = Number(myString);
+console.log(typeof myNum);
+
+
+var myNum = 123;
+var myString = myNum.toString();
+console.log(typeof myString);
