c740fe51a6e3b38aa5e978038eff65d6528cb73d
nelviCastellanos <nelvicas97@gmail.com>
2025-08-31 21:56:33 -0600

    tema condicionales

---
+++ 12-Condicionales /main.js
@@ -0,0 +1,22 @@
+///   Condicionales 
+
+// if ...  else 
+// sintaxis basica 
+/*
+if (condition) {
+  // code to run if condition is true 
+ } else {
+  // run some other code instead 
+}
+
+*/
+
+let shoppingDone = true;
+let childAllowance;
+
+if (shoppingDone === true) {
+  console.log(childAllowance = 10);
+} else {
+  console.log(childAllowance = 5);
+}
+
