f427d99670b446b64a18f088688d429f28b350a6
nelviCastellanos <nelvicas97@gmail.com>
2025-09-19 12:55:48 -0600

    tema creando funcion

--- 27-Construyendo_tu_funcion/main.js
+++ 27-Construyendo_tu_funcion/main.js
@@ -6,3 +6,34 @@
 alert("This is a message");
 
 */
+
+
+//  sintaxix de funcion basica 
+
+
+function displayMessage() {
+  const body = document.body;
+
+  const panel = document.createElement("div");
+  panel.setAttribute("class", "msgBox");
+  body.appendChild(panel);
+
+  const msg = document.createElement("p");
+  msg.textContent = "This is a message box";
+  panel.appendChild(msg);
+
+  const closeBtn = document.createElement("button");
+  closeBtn.textContent = "x";
+  panel.appendChild(closeBtn);
+
+  closeBtn.addEventListener("click", () =>
+    panel.parentNode.removeChild(panel),
+ );
+}
+
+
+// displayMessage();
+
+
+const btn = document.querySelector("button");
+btn.addEventListener("click", displayMessage);
