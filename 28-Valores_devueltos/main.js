2fae047b23bc54ca320bfe9751779d56a606f32e
nelviCastellanos <nelvicas97@gmail.com>
2025-09-21 20:52:37 -0600

    tema valores deueltos funciones

--- 28-Valores_devueltos/main.js
+++ 28-Valores_devueltos/main.js
@@ -2,3 +2,26 @@
 const myText = "The weather is cold";
 const newString = myText.replace("cold", "warm");
 console.log(newString);
+
+
+//// Funciones con alores de retorno
+
+
+function squared(num) {
+  return num * num;
+}
+
+function cubed(num) {
+  return num * num * num;
+}
+
+function factorial(num) {
+  if (num < 0) return undefined;
+  if (num === 0) return 1;
+  let x = num - 1;
+  while (x > 1) {
+    num *= x;
+    x--;
+  }
+  return num;
+}
