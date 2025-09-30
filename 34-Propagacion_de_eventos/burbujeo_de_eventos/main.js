600ab9b940cd4564e9a9bfc7ef6ad1a5dbbfb50a
nelviCastellanos <nelvicas97@gmail.com>
2025-09-29 18:58:20 -0600

    tema propagacion de eventos

---
+++ 34-Propagacion_de_eventos/burbujeo_de_eventos/main.js
@@ -0,0 +1,32 @@
+
+const output = document.querySelector("#output");
+function handleClick(e) {
+  output.textContent += `You clicked on a ${e.currentTarget.tagName} element\n`;
+}
+
+const container = document.querySelector("#container");
+container.addEventListener("click", handleClick);
+
+
+
+
+
+/*
+
+// solucionando el con stopPropagation()  la propagacion de eventos en ocasiones suele solucionar problemas
+
+
+const btn = document.querySelector("button");
+const box = document.querySelector("div");
+const video = document.querySelector("video");
+
+btn.addEventListener("click", () => box.classList.remove("hidden"));
+
+video.addEventListener("click", (event) => {
+  event.stopPropagation();
+  video.play();
+});
+
+box.addEventListener("click", () => box.classList.add("hidden"));
+
+*/
