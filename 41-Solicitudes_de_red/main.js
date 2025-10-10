d18b0b72d2db09b0d5539b9be4becc80eb598526
nelviCastellanos <nelvicas97@gmail.com>
2025-10-09 21:03:32 -0600

    tema solicitudes de red

--- 41-Solicitudes_de_red/main.js
+++ 41-Solicitudes_de_red/main.js
@@ -23,3 +23,7 @@ function updateDisplay(verse) {
     verse = verse.replace(" ", "").toLowerCase();
     const url = `${verse}.txt`;
 }
+
+
+updateDisplay("Verse 1");
+verseChoose.value = "Verse 1";
