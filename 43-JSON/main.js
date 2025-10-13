ce576c6dc1037827634d41308cd9b570b5b7b30a
nelviCastellanos <nelvicas97@gmail.com>
2025-10-12 22:36:01 -0600

    tema JSON

--- 43-JSON/main.js
+++ 43-JSON/main.js
@@ -1,1 +1,8 @@
 //  Formto de archivo Json  
+
+/*
+
+La notación de objetos JavaScript (JSON) es un formato estándar basado en texto para representar datos estructurados basados en la sintaxis de objetos JavaScript. Se usa comúnmente para transmitir datos en aplicaciones web (por ejemplo, enviar algunos datos desde el servidor al cliente, para que puedan mostrarse en una página web, o viceversa)
+
+
+*/
