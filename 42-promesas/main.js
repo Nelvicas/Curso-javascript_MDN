e043ffbfa756b0fb5b363e6bb1d393c702a2b735
nelviCastellanos <nelvicas97@gmail.com>
2025-10-19 21:26:31 -0600

    tema promesas

--- 42-promesas/main.js
+++ 42-promesas/main.js
@@ -65,5 +65,20 @@ fetchPromise3
 
   ///   Deteccion de errores
 
+const fetchPromise4 = fetch(
+  "bad-scheme://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json",
+);
 
-  
+fetchPromise4
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error(`HTTP error: ${response.status}`);
+    }
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data[0].name);
+  })
+  .catch((error) => {
+    console.error(`Could not get products: ${error}`);
+  });
