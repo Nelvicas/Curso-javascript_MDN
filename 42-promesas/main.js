334abe366a609554c81c117548c2030356ae9157
nelviCastellanos <nelvicas97@gmail.com>
2025-11-20 21:39:55 -0600

    tema promesas js

--- 42-promesas/main.js
+++ 42-promesas/main.js
@@ -62,7 +62,7 @@ fetchPromise3
 
 
 
-  ///   Deteccion de errores
+  ///   Deteccion de errores con uso de catch 
 
 const fetchPromise4 = fetch(
   "bad-scheme://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json",
@@ -78,6 +78,47 @@ fetchPromise4
   .then((data) => {
     console.log(data[0].name);
   })
-  .catch((error) => {
+  .catch((error) => {   /// con catch capturamos el error 
     console.error(`Could not get products: ${error}`);
+  });
+
+
+
+
+  ///    Terminologia de promesas 
+
+/*
+pendiente: El estado inicial. La operación aún no se ha completado (ni tuvo éxito ni fracasó).
+
+cumplida: La operación tuvo éxito. Es entonces cuando llaman al encargado de la promesa..then()
+
+rechazado: La operación fracasó. Es entonces cuando llaman al encargado de la promesa..catch()
+
+*/
+
+
+
+
+///   combinar  multiples promesas 
+
+
+
+const fetchPromise5 = fetch(
+  "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json",
+);
+const fetchPromise6 = fetch(
+  "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/not-found",
+);
+const fetchPromise7 = fetch(
+  "https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json",
+);
+
+Promise.all([fetchPromise5, fetchPromise6, fetchPromise7])
+  .then((responses) => {
+    for (const response of responses) {
+      console.log(`${response.url}: ${response.status}`);
+    }
+  })
+  .catch((error) => {
+    console.error(`Failed to fetch: ${error}`);
   });
